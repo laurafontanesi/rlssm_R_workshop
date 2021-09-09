70b3143e26b25e22c18448e3ddc8fc6af09b68d8
laurafontanesi <laura.fontanesi.1@gmail.com>
2021-09-09 14:13:51 +0200

    post pred

--- test.R
+++ test.R
@@ -29,48 +29,18 @@ ggplot(data = sim_path, aes(x = time, y = dv, color=factor(accumulator)))+
   geom_line(size = .5) +
   geom_hline(yintercept=gen_threshold, size=1.5)
 
-# fit hierarchical DM on real data
-data <- read.csv('data/fontanesi2019.csv')
-data <- select(data, -X) # drop pandas index column
-data <- data[data$participant < 6,] # select only 5 participants
-data$accuracy_recoded <- data$accuracy
-data[data$accuracy==0, "accuracy_recoded"] <- -1
+# posterior predictives
+sim_data <- rWaldRace(n=1000, v=c(.7, 1.2), B=1.5, A=0, t0=.23, gf = 0)
+summary(sim_data)
 
-data[(data$inc_option == 1)&(data$cor_option == 2),"condition_label"] <- "difficult_low"
-data[(data$inc_option == 1)&(data$cor_option == 3),"condition_label"] <- "easy_low"
-data[(data$inc_option == 2)&(data$cor_option == 4),"condition_label"] <- "easy_high"
-data[(data$inc_option == 3)&(data$cor_option == 4),"condition_label"] <- "difficult_high"
-
-data[(data$inc_option == 1)&(data$cor_option == 2),"condition"] <- 1
-data[(data$inc_option == 1)&(data$cor_option == 3),"condition"] <- 2
-data[(data$inc_option == 2)&(data$cor_option == 4),"condition"] <- 3
-data[(data$inc_option == 3)&(data$cor_option == 4),"condition"] <- 4
-
-ggplot(data = data, aes(x = condition, y = accuracy, color=condition_label))+
-  stat_summary(fun = "mean", geom="point",  size=5) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar",  size=.5, width=.3)
-
-ggplot(data = data, aes(x = condition, y = rt, color=condition_label))+
-  stat_summary(fun = "mean", geom="point",  size=5) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar",  size=.5, width=.3)
-
-head(data)
-
-unique(data$participant)
-
-sim_data_for_stan <- list(
-  N = dim(data)[1],
-  L = length(unique(data$participant)),
-  C = 4,
-  condition = data$condition,
-  participant = data$participant,
-  accuracy = data$accuracy_recoded,
-  rt = data$rt,
-  starting_point = 0.5
+sim_data_for_stan = list(
+  N = dim(sim_data)[1],
+  choices = sim_data$R,
+  rt = sim_data$RT
 )
 
 fit1 <- stan(
-  file = "stan_models/hierDDM_cond.stan",            # Stan program
+  file = "stan_models/RDM.stan",                     # Stan program
   data = sim_data_for_stan,                          # named list of data
   chains = 2,                                        # number of Markov chains
   warmup = 1000,                                     # number of warmup iterations per chain
@@ -78,30 +48,7 @@ fit1 <- stan(
   cores = 2                                          # number of cores (could use one per chain)
 )
 
-print(fit1, pars = c("mu_drift[1]", "mu_drift[2]", "mu_drift[3]", "mu_drift[4]",
-                     "sd_drift[1]", "sd_drift[2]", "sd_drift[3]", "sd_drift[4]",
-                     "mu_threshold[1]", "mu_threshold[2]", "mu_threshold[3]", "mu_threshold[4]",
-                     "sd_threshold[1]", "sd_threshold[2]", "sd_threshold[3]", "sd_threshold[4]",
-                     "mu_ndt", "sd_ndt"))
+print(fit1, pars = c("transf_drift1", "transf_drift2", "transf_threshold", "transf_ndt"))
 
-posterior <- as.matrix(fit1)
-
-plot_title <- ggtitle("Posterior distributions mu drift-rate per condition",
-                      "with medians and 95% intervals")
-mcmc_areas(posterior,
-           pars = c("mu_drift[1]", "mu_drift[2]", "mu_drift[3]", "mu_drift[4]"),
-           prob = 0.95) + plot_title
-
-plot_title <- ggtitle("Posterior distributions mu threshold per condition",
-                      "with medians and 95% intervals")
-
-mcmc_areas(posterior,
-           pars = c("mu_threshold[1]", "mu_threshold[2]", "mu_threshold[3]", "mu_threshold[4]"),
-           prob = 0.95) + plot_title
-
-plot_title <- ggtitle("Posterior distributions individual NDT parameters",
-                      "with medians and 95% intervals")
-
-mcmc_areas(posterior,
-           pars = c("ndt_sbj[1]", "ndt_sbj[2]", "ndt_sbj[3]", "ndt_sbj[4]"),
-           prob = 0.95) + plot_title
+# Extract single-trial drift-rates
+extract(fit1, paste("drift1_t[", seq(1, dim(sim_data)[1], 1), "]", sep = ''))
